e7d11234332dfe66578bf4b1a5621610ca3f3828
Fatima Rafiqui <fatima.rafiqui@gmail.com>
2015-10-09 23:28:29 +0530

    Create SearchElement.c

---
+++ LinkList/SearchElement.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int key;
+    struct node* next;
+};
+
+void push(struct node** head_ref, int new_key)
+{
+   
+    struct node* new_node =
+            (struct node*) malloc(sizeof(struct node));
+    new_node->key  = new_key;
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+}
+
+bool search(struct node* head, int x)
+{
+    struct node* current = head;  
+    while (current != NULL)
+    {
+        if (current->key == x)
+            return true;
+        current = current->next;
+    }
+    return false;
+}
+
+
+int main()
+{
+   
+    struct node* head = NULL;
+    int x = 21;
+    push(&head, 10);
+    push(&head, 30);
+    push(&head, 11);
+    push(&head, 21);
+    push(&head, 14);
+
+    search(head, 21)? printf("Yes") : printf("No");
+    return 0;
+}
